250b96aa97a810a9c6c0f11cfbcf780c3790b909
davedr <david.184@seznam.cz>
2023-10-06 16:37:13 +0200

    commented out prints

--- regtest_control.py
+++ regtest_control.py
@@ -10,7 +10,7 @@ class RegTest_Btccore_Constants():
 REGTEST_CONTROL_CONSTANTS = RegTest_Btccore_Constants()
 
 
-def create_wallet_btc_core(wallet_name : str):
+def create_wallet_btc_core(wallet_name: str):
     request = "{\"jsonrpc\": \"2.0\",\"method\": \"createwallet\",\"params\": [\"" + wallet_name + "\"]}"
     response = requests.post(REGTEST_CONTROL_CONSTANTS.bitcoin_regtest_rpc_url, data = request, auth=(REGTEST_CONTROL_CONSTANTS.btc_core_username, 
                                                                                             REGTEST_CONTROL_CONSTANTS.btc_core_pswd))
@@ -36,11 +36,11 @@ def get_block_count():
     response = requests.post(REGTEST_CONTROL_CONSTANTS.bitcoin_regtest_rpc_url, data = request, auth=(REGTEST_CONTROL_CONSTANTS.btc_core_username, 
                                                                                           REGTEST_CONTROL_CONSTANTS.btc_core_pswd))
     response = response.json()
-    print(response["result"])
+    #print(response["result"])
     return response["result"]
 
 
-def send_to_address_btc_core(address : str, amount : float):
+def send_to_address_btc_core(address: str, amount: float):
     request = "{{\"jsonrpc\": \"2.0\",\"method\": \"sendtoaddress\",\"params\": [\"{0}\", {1}]}}".format(address, amount)
     response = requests.post(REGTEST_CONTROL_CONSTANTS.bitcoin_regtest_rpc_url, data = request, auth=(REGTEST_CONTROL_CONSTANTS.btc_core_username, 
                                                                                           REGTEST_CONTROL_CONSTANTS.btc_core_pswd))
