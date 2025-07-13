c5d0a1a848337f7a48557a4e24c3a7ef398ef2f3
Petr Svenda <petr@svenda.com>
2025-07-13 08:18:12 +0200

    refactor: update script paths to new source structure

--- process_sw.sh
+++ process_sw.sh
@@ -4,35 +4,36 @@ BASE_PATH=$HOME
 TMP_DIR="$BASE_PATH/btc/dumplings_temp2"
 
 # Start processing in virtual environment
-source $BASE_PATH/btc/coinjoin-analysis/myenv/bin/activate 
+source $BASE_PATH/btc/coinjoin-analysis/venv/bin/activate 
 
 # Go to analysis folder with scripts
-cd $BASE_PATH/btc/coinjoin-analysis
+cd $BASE_PATH/btc/coinjoin-analysis/src
 
 # Extract and process Dumplings results
-python3 parse_dumplings.py --cjtype sw --action process_dumplings --target-path $TMP_DIR/ | tee parse_dumplings.py.log
+python3 -m cj_process.parse_dumplings --cjtype sw --action process_dumplings --target-path $TMP_DIR/ | tee parse_dumplings.py.log
 
 # Copy already known false positives from false_cjtxs.json
-for dir in whirlpool whirlpool_100k whirlpool_1M whirlpool_5M whirlpool_50M; do
+for dir in whirlpool whirlpool_100k whirlpool_1M whirlpool_5M whirlpool_50M whirlpool_ashigaru_25M whirlpool_ashigaru_2_5M; do
     cp $BASE_PATH/btc/coinjoin-analysis/data/whirlpool/false_cjtxs.json $TMP_DIR/Scanner/$dir/
 done
 
 # Download historical fee rates
 curl -sSL "https://mempool.space/api/v1/mining/blocks/fee-rates/all" > $TMP_DIR/Scanner/whirlpool/fee_rates.json
-curl -sSL "https://mempool.space/api/v1/mining/blocks/fee-rates/all" > $TMP_DIR/Scanner/whirlpool_100k/fee_rates.json
-curl -sSL "https://mempool.space/api/v1/mining/blocks/fee-rates/all" > $TMP_DIR/Scanner/whirlpool_1M/fee_rates.json
-curl -sSL "https://mempool.space/api/v1/mining/blocks/fee-rates/all" > $TMP_DIR/Scanner/whirlpool_5M/fee_rates.json
-curl -sSL "https://mempool.space/api/v1/mining/blocks/fee-rates/all" > $TMP_DIR/Scanner/whirlpool_50M/fee_rates.json
+for dir in whirlpool_100k whirlpool_1M whirlpool_5M whirlpool_50M whirlpool_ashigaru_25M whirlpool_ashigaru_2_5M; do
+    cp $TMP_DIR/Scanner/whirlpool/fee_rates.json $TMP_DIR/Scanner/$dir/fee_rates.json
+done
 
 # Run false positives detection
-python3 parse_dumplings.py --cjtype sw --action detect_false_positives --target-path $TMP_DIR/ | tee parse_dumplings.py.log
+python3 -m cj_process.parse_dumplings --cjtype sw --action detect_false_positives --target-path $TMP_DIR/ | tee parse_dumplings.py.log
 
+# Analyse liquidity 
+python3 -m cj_process.parse_dumplings --cjtype sw --target-path $TMP_DIR/ --env_vars "ANALYSIS_LIQUIDITY=True" | tee parse_dumplings.py.log
 
 # Run generation of aggregated plots 
-python3 parse_dumplings.py --cjtype sw --action plot_coinjoins --env_vars "PLOT_REMIXES_MULTIGRAPH=False" --target-path $TMP_DIR/ | tee parse_dumplings.py.log
+python3 -m cj_process.parse_dumplings --cjtype sw --action plot_coinjoins --env_vars "PLOT_REMIXES_MULTIGRAPH=False" --target-path $TMP_DIR/ | tee parse_dumplings.py.log
 
 # Run generation of plots only for specific intervals
-python3 parse_dumplings.py --cjtype sw --action plot_coinjoins --target-path $TMP_DIR/ --env_vars "PLOT_REMIXES_SINGLE_INTERVAL=True" | tee parse_dumplings.py.log
+python3 -m cj_process.parse_dumplings --cjtype sw --action plot_coinjoins --target-path $TMP_DIR/ --env_vars "PLOT_REMIXES_SINGLE_INTERVAL=True" | tee parse_dumplings.py.log
 
 
 #
