769e087005676567edf5f6ca545346343cd25d41
Petr Svenda <petr@svenda.com>
2025-07-16 18:09:09 +0200

    ci: update scripts

--- process_ww2.sh
+++ process_ww2.sh
@@ -7,16 +7,6 @@ TMP_DIR="$BASE_PATH/btc/dumplings_temp2"
 # Start processing in virtual environment
 source $BASE_PATH/btc/coinjoin-analysis/venv/bin/activate 
 
-
-# Remove previous temporary directory
-rm -rf $TMP_DIR/
-
-# Create new temporary directory
-mkdir $TMP_DIR/
-
-# Unzip processed dumplings files
-unzip $BASE_PATH/btc/dumplings.zip -d $TMP_DIR/
-
 # Go to analysis folder with scripts
 cd $BASE_PATH/btc/coinjoin-analysis/src
 
@@ -88,45 +78,3 @@ python3 -m cj_process.parse_dumplings --cjtype ww2 --target-path $TMP_DIR/ --env
 python3 -m cj_process.file_check $TMP_DIR/Scanner/  | tee parse_dumplings.py.log
 
 
-
-
-#
-# Backup outputs
-#
-DEST_DIR="/data/btc/dumplings_archive/results_$(date +%Y%m%d)"
-echo "(Re-)creating $DEST_DIR"
-rm -rf "$DEST_DIR"
-mkdir -p "$DEST_DIR"
-
-# Get the absolute paths of source and destination
-SOURCE_DIR=$(realpath "$TMP_DIR")
-DEST_DIR=$(realpath "$DEST_DIR")
-
-# Use find to locate all .json files except info_*.json and copy them while preserving structure
-find "$TMP_DIR" -type f \( -name "*.json" -o -name "*.pdf" -o -name "*.png" -o -name "coinjoin_results_check_summary.txt" \) ! -name "coinjoin_tx_info*.json" ! -name "*_events.json"| while read -r file; do
-    # Compute relative path
-    REL_PATH="${file#$SOURCE_DIR/}"
-    # Create target directory if it does not exist
-    mkdir -p "$DEST_DIR/$(dirname "$REL_PATH")"
-    # Copy file
-    cp "$file" "$DEST_DIR/$REL_PATH"
-    #echo "Copying $file to $DEST_DIR/$REL_PATH"
-done
-
-echo "Selected files archived to: $DEST_DIR"
-
-# Create montage from multiple selected images
-DEST_DIR="/data/btc/dumplings_archive/results_$(date +%Y%m%d)"
-image_list=""
-for pool in others kruw gingerwallet opencoordinator coinjoin_nl wasabicoordinator wasabist dragonordnance mega btip; do
-    pool_PATH="$DEST_DIR/Scanner/wasabi2_$pool/wasabi2_${pool}_cummul_values_norm.png"
-    image_list="$image_list $pool_PATH"
-done
-#for pool in others kruw gingerwallet opencoordinator; do
-#    pool_PATH="$DEST_DIR/Scanner/wasabi2_$pool/wasabi2_${pool}_input_types_values_norm.png"
-#    image_list="$image_list $pool_PATH"
-#done
-montage $image_list -tile 2x -geometry +2+2 $DEST_DIR/Scanner/wasabi2/wasabi2_tiles_all_cummul_values_norm.png
-
-# Upload selected files (separate scripts, can be configured based on desired upload service)
-./upload_ww2.sh
